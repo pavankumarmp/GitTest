85cbaa8d80840e4b490904079b14b391a24415b9
pavan <pavan2k15kumar@gmail.com>
2015-08-26 14:37:18 +0530

    First Commit

---
+++ GitTest/src/gittest/Main.java
@@ -0,0 +1,13 @@
+package gittest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ShowMsg a=new ShowMsg();
+		a.Show();
+		a.Disp();
+		
+
+	}
+
+}
---
+++ GitTest/src/gittest/ShowMsg.java
@@ -0,0 +1,12 @@
+package gittest;
+
+public class ShowMsg {
+	void Show(){
+		System.out.println("This is inside Show");
+	}
+	void Disp()
+	{
+		System.out.println("This is disp");
+	}
+
+}
