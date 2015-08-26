7f81e779503926382e5df26428356f89a7eabcfb
pavan <pavan2k15kumar@gmail.com>
2015-08-26 14:42:42 +0530

    Edited Show()

--- GitTest/src/gittest/ShowMsg.java
+++ GitTest/src/gittest/ShowMsg.java
@@ -3,6 +3,11 @@ package gittest;
 public class ShowMsg {
 	void Show(){
 		System.out.println("This is inside Show");
+		int []a=new int[5];
+		for (int i=0;i<a.length;i++)
+		{
+			System.out.println(" "+a[i]);
+		}
 	}
 	void Disp()
 	{
