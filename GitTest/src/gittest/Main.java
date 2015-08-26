2fc9a70ca7eb7bdc2a9b6c9b3cca931533ca67b1
pavan <pavan2k15kumar@gmail.com>
2015-08-26 15:32:55 +0530

    changed to upper

--- GitTest/src/gittest/Main.java
+++ GitTest/src/gittest/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		ShowMsg a=new ShowMsg();
-		System.out.println("hello");
+		System.out.println("HELLO");
 		a.Show();
 		a.Disp();
 		
